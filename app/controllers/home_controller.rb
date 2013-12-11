eca782b3332bc0f2678f2f7840ce70cf14ddaa67
Jeff Zamanski <jzamansk@gmu.edu>
2013-12-11 05:26:34 -0500

    Don't require authentication for Home#Index action.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: :instructions
+  skip_before_action :authenticate_user!, only: [:index, :instructions]
 
+  def index
+  end
+  
   def play
     unless current_user.games.active.count == 0
       redirect_to(game_path(current_user.games.active.first))
