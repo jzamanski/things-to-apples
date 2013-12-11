ebd4c3116e408d12e071555cbf2418c9ed99972a
Jeff Zamanski <jzamansk@gmu.edu>
2013-12-11 01:06:49 -0500

    Change production DB to MySQL and secure secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ThingsToApples::Application.config.secret_key_base = '195a565e7d2d5011c4022a65bfc1d21e59d619e5e182ac045486c503f9e740ea3e7f9c568567c77229fead69043a40d1763ff57d798289fc473ed394e4d8c37d'
+
+if Rails.env.production?
+  ThingsToApples::Application.config.secret_key_base = ENV["SECRET_KEY"]
+elsif Rails.env.development? or Rails.env.test?
+  ThingsToApples::Application.config.secret_key_base = 'x'
+end
